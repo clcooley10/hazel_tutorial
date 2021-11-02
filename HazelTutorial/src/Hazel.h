8b5fc33e6b9fa72709c6fe47bc196eb28b200ae6
Christopher Cooley <clcooley@umich.edu>
2021-11-02 11:43:47 -0600

    Up Through Video 20

--- HazelTutorial/src/Hazel.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// For use by Hazel Applications
-#include "Hazel/Application.h"
-#include <stdio.h>
-
-//-----Entry Point-------------------
-#include "Hazel/EntryPoint.h"
-//-----------------------------------
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,26 @@
 #include <Hazel.h>
 
+class ExampleLayer : public Hazel::Layer {
+public:
+	ExampleLayer() : Layer("Example") {}
+
+	void OnUpdate() override {
+		if (Hazel::Input::IsKeyPressed(HZ_KEY_TAB)) { HZ_TRACE("Tab key pressed"); }
+	}
+	void OnEvent(Hazel::Event& event) override {
+		if (event.GetEventType() == Hazel::EventType::KeyPressed) {
+			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
+			HZ_TRACE("{0}", (char)e.GetKeyCode());
+		}
+	}
+};
+
 class Sandbox : public Hazel::Application {
 public:
-	Sandbox() {}
+	Sandbox() { 
+		PushLayer(new ExampleLayer()); 
+		PushOverlay(new Hazel::ImGuiLayer());
+	}
 	~Sandbox() {}
 };
 
